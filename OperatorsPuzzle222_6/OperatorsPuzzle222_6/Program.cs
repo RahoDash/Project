bbeffb9712879c87f932ddcb4875256c468cbc91
besmir <besmir.slk@eduge.ch>
2017-09-25 23:16:18 +0200

    Update operatorz

--- OperatorsPuzzle222_6/OperatorsPuzzle222_6/Program.cs
+++ OperatorsPuzzle222_6/OperatorsPuzzle222_6/Program.cs
@@ -8,34 +8,27 @@ namespace OperatorsPuzzle222_6
 {
     class Program
     {
+        static readonly char[] OPERATORS = { '+', '-', '*', '/' };
+        static readonly int[] OPERATORS_PRIORITY = { 12, 13 };
+        static readonly Operator[] OPERATORS_RESULT = { new Operator(OPERATORS[0], OPERATORS_PRIORITY[0], (a, b) => a + b),
+                                                        new Operator(OPERATORS[1], OPERATORS_PRIORITY[0], (a, b) => a - b),
+                                                        new Operator(OPERATORS[2], OPERATORS_PRIORITY[1], (a, b) => a * b),
+                                                        new Operator(OPERATORS[3], OPERATORS_PRIORITY[1], (a, b) => a / b)};
+        static readonly int NUMB_OPERATOR = OPERATORS.Length;
         const int MIN_NUMBER = 2;
         const int MAX_NUMBER = 6;
-        const int NUMB_OPERATOR = 4;
         const int TARGET = 6;
-        static readonly char[] OPERATORS = { '+', '-', '*', '/' };
-        static readonly int[] OPERATORS_PRIORITY = { 12, 12, 13, 13 };
-        static readonly Operator[] OPERATORS_RESULT = { new Operator(OPERATORS[0], 12, (a, b) => a + b),
-                                                        new Operator(OPERATORS[1], 12, (a, b) => a - b),
-                                                        new Operator(OPERATORS[2], 12, (a, b) => a * b),
-                                                        new Operator(OPERATORS[3], 12, (a, b) => a / b)};
 
         static void Main(string[] args)
         {
             int numberFound;
-
-            //Operator add = new Operator(OPERATORS[0], 12, (a, b) => a + b);
-            //Operator sub = new Operator(OPERATORS[1], 12, (a, b) => a - b);
-            //Operator mul = new Operator(OPERATORS[2], 13, (a, b) => a * b);
-            //Operator div = new Operator(OPERATORS[3], 13, (a, b) => a / b);
-
-
             for (int i = MIN_NUMBER; i <= MAX_NUMBER; i++)
             {
                 for (int j = 0; j < NUMB_OPERATOR; j++)
                 {
                     for (int k = 0; k < NUMB_OPERATOR; k++)
                     {
-                        if (OPERATORS_PRIORITY[j] < OPERATORS_PRIORITY[k])
+                        if (OPERATORS_RESULT[j].Priority < OPERATORS_RESULT[k].Priority)
                         {
                             numberFound = OPERATORS_RESULT[j].Op(i, OPERATORS_RESULT[k].Op(i, i));
                         }
@@ -50,7 +43,6 @@ namespace OperatorsPuzzle222_6
                     }
                 }
             }
-
         }
     }
 }
