f978b031a6fef7b691cf1055d8142c5be306452f
besmir <besmir.slk@eduge.ch>
2017-09-26 21:42:44 +0200

    Commented operatorz

--- OperatorsPuzzle222_6/OperatorsPuzzle222_6/Operator.cs
+++ OperatorsPuzzle222_6/OperatorsPuzzle222_6/Operator.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/***
+* Name     :   Operator Puzzle 2 () 2 () 2 = 6
+* Author   :   Besmir Silka
+* Desc.    :   Class Operator, This will only Init the constructor and the delgate methode
+* Version  :   1.0, 26.09.2017, B.SILKA  
+***/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,19 +13,36 @@ using System.Threading.Tasks;
 
 namespace OperatorsPuzzle222_6
 {
+    /// <summary>
+    /// Signature of the delegate function
+    /// </summary>
+    /// <param name="a">Require an integer 32-bit</param>
+    /// <param name="b">Require an integer 32-bit</param>
+    /// <returns></returns>
     delegate int MathAction(int a, int b);
 
     class Operator
     {
-
+        /// <summary>
+        /// Fields
+        /// </summary>
         MathAction _op;
         char _nameOp;
         int _priority;
 
+        /// <summary>
+        /// Propreties
+        /// </summary>
         public char NameOp { get => _nameOp; set => _nameOp = value; }
         public int Priority { get => _priority; set => _priority = value; }
         internal MathAction Op { get => _op; set => _op = value; }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="nameOp">Will set the operator to use</param>
+        /// <param name="prior">Is the priority of the operator</param>
+        /// <param name="op">will be delgated on call</param>
         public Operator(char nameOp, int prior, MathAction op)
         {
             NameOp = nameOp;
--- OperatorsPuzzle222_6/OperatorsPuzzle222_6/Program.cs
+++ OperatorsPuzzle222_6/OperatorsPuzzle222_6/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/***
+* Name     :   Operator Puzzle 2 () 2 () 2 = 6
+* Author   :   Besmir Silka
+* Desc.    :   Program Operator, The result of the same number through different operator must be result to the target,
+*                                with the priority of the operator to take in consideration.    
+* Version  :   1.0, 26.09.2017, B.SILKA  
+***/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +16,26 @@ namespace OperatorsPuzzle222_6
 {
     class Program
     {
-        static readonly char[] OPERATORS = { '+', '-', '*', '/' };
-        static readonly int[] OPERATORS_PRIORITY = { 12, 13 };
+        /// <summary>
+        /// Constant
+        /// </summary>
+        static readonly char[] OPERATORS = { '+', '-', '*', '/' }; //The array of operators
+        static readonly int[] OPERATORS_PRIORITY = { 12, 13 }; // The array priority of the operator, the lesser the higher the priority is
         static readonly Operator[] OPERATORS_RESULT = { new Operator(OPERATORS[0], OPERATORS_PRIORITY[0], (a, b) => a + b),
                                                         new Operator(OPERATORS[1], OPERATORS_PRIORITY[0], (a, b) => a - b),
                                                         new Operator(OPERATORS[2], OPERATORS_PRIORITY[1], (a, b) => a * b),
-                                                        new Operator(OPERATORS[3], OPERATORS_PRIORITY[1], (a, b) => a / b)};
-        static readonly int NUMB_OPERATOR = OPERATORS.Length;
-        const int MIN_NUMBER = 2;
+                                                        new Operator(OPERATORS[3], OPERATORS_PRIORITY[1], (a, b) => a / b)}; // An array of methodes, with a delgated method
+        static readonly int NUMB_OPERATOR = OPERATORS.Length; // Numbers of record on the "OPERATORS_RESULT"
+        //The operation will go to MIN_NUMBER to MAX_NUMBER
+        const int MIN_NUMBER = 2; 
         const int MAX_NUMBER = 6;
+
+        //This will be the target of the operation
         const int TARGET = 6;
 
         static void Main(string[] args)
         {
+            //this will be compared to the target to accept the result
             int numberFound;
             for (int i = MIN_NUMBER; i <= MAX_NUMBER; i++)
             {
