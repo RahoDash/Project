02362fe48e8682c204e113ae53bac52e0ea6170f
Unknown <besmir.slk@eduge.ch>
2017-10-09 08:10:20 +0200

    Project fizzbuzz interfaces

---
+++ ExFizzBuzzTDD/ExFizzBuzzTDD/FizzBuzz.cs
@@ -0,0 +1,42 @@
+﻿namespace ExFizzBuzzTDD
+{
+    public class FizzBuzz : IRules
+    {
+
+        public string IsBuzz(int num)
+        {
+            if (num%5==0)
+            {
+                return "Buzz";
+            }
+            return string.Empty;
+        }
+        public string IsFizz(int num)
+        {
+            if (num%3==0)
+            {
+                return "Fizz";
+            }
+            return string.Empty;
+        }
+
+        public bool IsEmpty(string result)
+        {
+            if (result == string.Empty)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string Apply(int num)
+        {
+            string result = string.Empty;
+            result += IsFizz(num);
+            result += IsBuzz(num);
+            result += IsEmpty(result) ? num.ToString(): "";
+
+            return result;
+        }
+    }
+}
---
+++ ExFizzBuzzTDD/ExFizzBuzzTDDTests/FizzBuzzTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExFizzBuzzTDD.Tests
+{
+    [TestClass()]
+    public class FizzBuzzTests
+    {
+
+        [TestMethod()]
+        public void FizzBuzzCtorTest1()
+        {
+            FizzBuzz target = new FizzBuzz();
+            Assert.AreNotEqual(null, target);
+        }
+
+        [TestMethod()]
+        public void FizzBuzzCtorTest2()
+        {
+            FizzBuzz target = new FizzBuzz();
+            Assert.AreEqual("1", target.Apply(1));
+        }
+
+        [TestMethod()]
+        public void FizzBuzzCtorTest3()
+        {
+            FizzBuzz target = new FizzBuzz();
+            Assert.AreEqual("2", target.Apply(2));
+        }
+
+        [TestMethod()]
+        public void FizzBuzzCtorTest4()
+        {
+            FizzBuzz target = new FizzBuzz();
+            Assert.AreEqual("Fizz", target.Apply(3));
+        }
+
+        [TestMethod()]
+        public void FizzBuzzCtorTest5()
+        {
+            FizzBuzz target = new FizzBuzz();
+            Assert.AreEqual("4", target.Apply(4));
+        }
+
+        [TestMethod()]
+        public void FizzBuzzCtorTest6()
+        {
+            FizzBuzz target = new FizzBuzz();
+            Assert.AreEqual("Buzz", target.Apply(5));
+        }
+    }
+}
