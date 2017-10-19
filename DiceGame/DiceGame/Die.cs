ab31d12d697d9733ec11b8544b8a19aebba4dba6
Unknown <besmir.slk@eduge.ch>
2017-10-19 16:07:55 +0200

    DiceGame init

---
+++ DiceGame/DiceGame/Die.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGame
+{
+    class Die
+    {
+        //private readonly string[] DEFAULT_FACES = {"1", "2", "3", "4", "5", "6"};
+
+        
+
+        public Die()
+        {
+
+        }
+
+
+    }
+}
---
+++ DiceGame/DiceGame/Symbol.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGame
+{
+
+
+    public class Symbol
+    {
+        const string DEFAULT_SYMBOL = "NONE";
+
+        /// <summary>
+        /// Private Fields
+        /// </summary>
+        private string _content;
+
+        public string Content { get => _content; set => _content = value; }
+
+        /// <summary>
+        /// CTOR of Symbol
+        /// </summary>
+        /// <param name="index"></param>
+        public Symbol() : this(DEFAULT_SYMBOL){}
+        public Symbol(string symb)
+        {
+            Content= symb;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>The value of the array</returns>
+        public string Display()
+        {
+            return Content;
+        }
+
+    }
+}
